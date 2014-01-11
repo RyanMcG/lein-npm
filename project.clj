ce683665de4f5516ca833c6da406e18069ed8610
Bodil Stokke <bodil@bodil.org>
2014-01-11 14:45:05 +0000

    Bump for 0.2.0 release.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-npm "0.1.0"
+(defproject lein-npm "0.2.0"
   :description "Manage Node dependencies for CLJS projects"
   :url "https://github.com/bodil/lein-npm"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[cheshire "5.0.1"]]
+  :dependencies [[cheshire "5.2.0"]]
   :eval-in-leiningen true)
