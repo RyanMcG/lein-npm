2c4c83bd22ca0a3e3b0c21aea4df87be623e7b3b
Ryan McGowan <ryan@ryanmcg.com>
2017-04-14 19:56:28 -0700

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-npm "0.6.2"
+(defproject lein-npm "0.7.0-rc1"
   :description "Manage npm dependencies for CLJS projects"
   :url "https://github.com/RyanMcG/lein-npm"
   :license {:name "Apache License, version 2.0"
